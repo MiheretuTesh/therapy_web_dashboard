e1821e35d9f29b86dfb00d66520d426e63bd08f1
MiheretuTesh <se.miheretu.degebassa@gmail.com>
2024-08-10 22:56:34 +0300

    feat: Friend page, Friends Profile Block added

---
+++ src/Pages/FriendList/FriendList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ContentWrapper } from "../../Components/Shared";
+import Layout from "../../Components/FriendList";
+
+const FriendListPage: React.FC = () => {
+  return (
+    <ContentWrapper>
+      <Layout />
+    </ContentWrapper>
+  );
+};
+
+export default FriendListPage;

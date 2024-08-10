ad7ad9d1edeab1b0903dfc8d734536c5a1542d0f
MiheretuTesh <se.miheretu.degebassa@gmail.com>
2024-08-10 22:59:37 +0300

    app route updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import { AuthPage, WelcomePage, HomePage, ChatRooms } from "./Pages";
+import {
+  AuthPage,
+  WelcomePage,
+  HomePage,
+  ChatRooms,
+  FriendList,
+  Therpiests,
+} from "./Pages";
 import { Routes as RoutesLink } from "./constants/routes";
 
 const App: React.FC = () => {
@@ -16,6 +23,8 @@ const App: React.FC = () => {
           <Route path={RoutesLink.recoveryPassword} element={<AuthPage />} />
           <Route path={RoutesLink.homePage} element={<HomePage />} />
           <Route path={RoutesLink.chatRooms} element={<ChatRooms />} />
+          <Route path={RoutesLink.friendList} element={<FriendList />} />
+          <Route path={RoutesLink.therpiests} element={<Therpiests />} />
           <Route path="*" element={<Navigate to={RoutesLink.logIn} />} />
         </Routes>
       </BrowserRouter>
--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -2,5 +2,7 @@ import WelcomePage from "./Welcome";
 import AuthPage from "./Auth";
 import HomePage from "./Home";
 import ChatRooms from "./ChatRooms";
+import FriendList from "./FriendList";
+import Therpiests from "./Therpiests";
 
-export { WelcomePage, AuthPage, HomePage, ChatRooms };
+export { WelcomePage, AuthPage, HomePage, ChatRooms, FriendList, Therpiests };
